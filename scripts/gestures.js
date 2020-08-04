e670cb932f70c8f69fff010ead177b5bbd44b90a
Alfredo <alfredomef@gmail.com>
2020-08-04 18:50:14 -0500

    Se integra funcionalidad de deslizar para movil

--- scripts/gestures.js
+++ scripts/gestures.js
@@ -1,4 +1,15 @@
+delete Hammer.defaults.cssProps.userSelect //habilita que el usario pueda selecionar el texto de los elementos donde se emplea hammer
+
 const body = document.getElementById('body')
 
 const hammerjs = new Hammer(body)
-hammerjs.on('pan', ev => console.log(ev))
+hammerjs.on('panleft panright', ev => {
+    if(ev.pointerType === 'touch'){
+        if(ev.type === 'panleft' && ev.distance > 100){
+            formElement.classList.add('active')
+        }
+        if(ev.type === 'panright' && ev.distance > 100){
+            formElement.classList.remove('active')
+        }
+    }
+})
