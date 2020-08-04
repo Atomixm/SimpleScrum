99fd0d02e105978fd822fc2d599cfc4fb194a935
Alfredo <alfredomef@gmail.com>
2020-08-04 17:49:34 -0500

    Se limpia el html, y se integra validacion de la api para la impresion en el board scrum

--- scripts/petitions.js
+++ scripts/petitions.js
@@ -10,9 +10,29 @@ function fillTasks (data){
         let newTask = document.createElement('article')
         let taskTitle = document.createElement('h3')
         taskTitle.innerText = d.title
+
+        let taskPerson = document.createElement('p')
+        taskPerson.innerHTML = `Responsable: <span>${d.person}</span>`
+
+        let taskDeadline = document.createElement('p')
+        taskDeadline.innerHTML = `Plazo: <span>${d.deadline-d.created}</span>`
+
         newTask.appendChild(taskTitle)
+        newTask.appendChild(taskPerson)
+        newTask.appendChild(taskDeadline)
+
+        let columTodo = document.getElementById('todoTasks')
+        let columInProgress = document.getElementById('inProgressTasks')
+        let columDone = document.getElementById('doneTasks')
         
-        let colum = document.getElementById('doneTasks')
-        colum.appendChild(newTask)
+        if(d.state == 'to-do'){
+            columTodo.appendChild(newTask)
+        }
+        if(d.state == 'in-progress'){
+            columInProgress.appendChild(newTask)
+        }
+        if(d.state == 'done'){
+            columDone.appendChild(newTask)
+        }
     })
 }
